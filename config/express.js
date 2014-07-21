d316f3eae799ce98f7a93981e97ee03fe2c9ff2d
pagrawl3 <mail@prath.am>
2014-07-21 22:14:40 +0530

    added favicon, made some speed optimizations

--- config/express.js
+++ config/express.js
@@ -5,18 +5,22 @@ var express = require('express'),
 module.exports = function (app) {
 
 	app.set('showStackError', true)
+	app.use(express.compress());
 	//Setup the public serving directory
-	app.use(express.static(rootPath + '/assets/public'))
+	app.use(express.staticCache());
+	app.use(express.static(rootPath + '/assets/public', {maxAge: 864000000}));
+	app.use(express.favicon(rootPath + '/assets/public/img/favicon.ico')); 
 	//Set the view templating engine and the views directory
-	app.set('views', rootPath + '/app/views')
-	app.set('view engine', 'jade')
+	app.set('views', rootPath + '/app/views');
+	app.set('view engine', 'jade');
 
 	//Default app configurations
 	app.configure(function() {
 		//bodyParser
-		app.use(express.bodyParser())
-		app.use(express.methodOverride())
+		app.use(express.json());
+		app.use(express.urlencoded());
+		app.use(express.methodOverride());
 		//Use the router
-		app.use(app.router)
+		app.use(app.router);
 	})
 }
